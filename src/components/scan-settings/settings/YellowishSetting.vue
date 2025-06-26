2c3d1d2f07f17775ab5b8085d3e226183dee36e2
zhjoven <github@zhuang95.com>
2025-06-26 22:04:27 +0800

    Update YellowishSetting.vue

--- src/components/scan-settings/settings/YellowishSetting.vue
+++ src/components/scan-settings/settings/YellowishSetting.vue
@@ -1,7 +1,7 @@
 <template>
   <n-form-item :label="t('settings.yellowish')" :show-feedback="false">
     <template #label>
-      <span :style="style" class="white-variance-label">
+      <span class="white-variance-label">
         {{ t('settings.yellowish') }}
       </span>
     </template>
@@ -10,7 +10,6 @@
 </template>
 
 <script lang="ts" setup>
-import { computed } from 'vue'
 import { NFormItem, NSlider } from 'naive-ui'
 import { useI18n } from 'vue-i18n'
 import { useVModel } from '@vueuse/core'
@@ -28,16 +27,6 @@ const emit = defineEmits<{
 }>()
 
 const yellowish = useVModel(props, 'yellowish', emit)
-
-const style = computed(() => {
-  const value = Math.max(0, Math.min(1, yellowish.value))
-  const [r, g, b] = [255, 255, 255].map((start, i) =>
-    Math.round(start + ([252, 242, 199][i] - start) * value)
-  )
-  return {
-    color: `rgb(${r}, ${g}, ${b})`
-  }
-})
 </script>
 
 <style scoped>
